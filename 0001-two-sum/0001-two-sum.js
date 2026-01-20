a15d212895f1ffabf383422aad9b079aca5b468e
Shubham Agrawal <ag.shubham747@gmail.com>
2026-01-21 01:24:29 +0530

    Time: 6 ms (46.02%), Space: 56.7 MB (26.75%) - LeetHub

--- 0001-two-sum/0001-two-sum.js
+++ 0001-two-sum/0001-two-sum.js
@@ -4,15 +4,37 @@
  * @return {number[]}
  */
 var twoSum = function(nums, target) {
-    for(let i=0; i< nums.length; i++)
+
+    // // # Brute force Approach
+    // for(let i=0; i< nums.length; i++)
+    // {
+    //     for( let j=i+1; j< nums.length; j++)
+    //     {
+    //         if(nums[j]=== target-nums[i])
+    //         {
+    //             return [i,j]
+    //         }
+    //     }
+    // }
+    // return []
+
+
+    // // Optimized Approach: using map(hash table)
+    // First we need to make a hash table using map that stores elements and its index respectively.
+    const map=new Map()
+    for(let i=0; i<=nums.length; i++)
     {
-        for( let j=i+1; j< nums.length; j++)
+        map.set(nums[i], i)
+    }
+    // Now after the hash table is ready, we w8ill implement the logic.
+    
+    for(let i=0; i<=nums.length; i++)
+    {
+        const complement= target- nums[i]
+        if(map.has(complement) && map.get(complement)!== i)
         {
-            if(nums[j]=== target-nums[i])
-            {
-                return [i,j]
-            }
+            return [i, map.get(complement)]
         }
     }
-    return []
+return []
 };
